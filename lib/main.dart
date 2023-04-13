51f2cfa69feb8f39125db986a5ca47e1a40931c0
hmmunioz <goticomananero@hotmail.com>
2023-04-13 04:31:38 -0500

    add readme

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: TextUIValues.tipti_test,
+      title: TextUIValues.tiptiTest,
       theme: ThemeData(),
       debugShowCheckedModeBanner: false,
       home: const PeoplePage(),
